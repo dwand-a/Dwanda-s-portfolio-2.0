82b82223bac550417e218ade8cbec2965f7823fc
Tahjay Ulett <tahjayulett@gmail.com>
2026-03-05 21:08:06 -0500

    Updated Vite.Config.JS

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/dwanda-s-portfolio/',   // ← this is the key fix (include the leading & trailing slash)
   root: '.',
   server: {
     open: true
