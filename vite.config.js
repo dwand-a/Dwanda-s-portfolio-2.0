f4bd23813117af366631ec16ec49121a221dca2b
Tahjay Ulett <tahjayulett@gmail.com>
2026-03-05 21:22:39 -0500

    fixed the repo name in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dwanda-s-portfolio-2.0/',   // ← this is the key fix (include the leading & trailing slash)
+  base: '/Dwanda-s-portfolio-2.0/',   // ← this is the key fix (include the leading & trailing slash)
   root: '.',
   server: {
     open: true
